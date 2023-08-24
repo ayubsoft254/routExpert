1c569852762529525adce08c0f44e29cbb2f0b30
ayubsoft254 <henryoayub15@gmail.com>
2023-08-25 00:13:59 +0300

    frontend building

--- frontend/routexpert-frontend/src/App.jsx
+++ frontend/routexpert-frontend/src/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import Body from './components/body/Body';
 import Footer from './components/footer/Footer';
-import signup from './components/signup/Signup';
+import SignUpPage from './components/signup/SignUpPage';
 
 const App = () => {
   return (
-    <> 
+    <Router> {/* Place the Router here */}
+      <>
         <Header />
         <Nav />
-        <Body />       
+        <Body />
         <Footer />
-        <signup />
-
-    </>
-  )
+        <Routes> {/* Use Routes instead of Switch */}
+          <Route path="./components/signup/SignUpPage" element={<SignUpPage />} />
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
 export default App;
+
